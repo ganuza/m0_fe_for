d673f0246907e0e6f41b8aeeb90c344aef45ce1c
Adrian Zabolitzki <adrianzab@gmail.com>
2023-04-19 21:04:11 -0600

    add challenge 3

--- for.js
+++ for.js
@@ -3,11 +3,12 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-for (var i = 25; i <= 75; i ++) {
+for (var i =25; i <= 75; i ++) {
   if (i % 2 == 0) {
     console.log(i);
   }
 }
+
 
 
 // Challenge 2
@@ -20,7 +21,11 @@ for (var i = 0; i < weather_options.length; i ++) {
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
-
+for (var i = 0; i < words.length; i ++) {
+  if (words[i].length === 3) {
+    console.log(words[i]);
+  }
+}
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
   // Use technical vocabulary and explain each part to demonstrate your understanding! 
